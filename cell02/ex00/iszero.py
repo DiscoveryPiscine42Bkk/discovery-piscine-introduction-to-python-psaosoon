f595c389d15105aed38621fa10b21325ac314fe2
psaosoon <phom976@gmail.com>
2025-06-20 02:02:04 +0000

    iszero.py

--- cell02/ex00/iszero.py
+++ cell02/ex00/iszero.py
@@ -1,6 +1,6 @@
-number = input(' ')
-number = int(number)
+number = float(input("Please enter a number: "))
+
 if number == 0:
-    print("This number is equal to zero.")
+  print("This number is equal to zero.")
 else:
-    print("This number is different from zero.")
+  print("This number is different from zero.")
