e89cfa039faacd24a46af0859ab2dcd322fafe0f
psaosoon <phom976@gmail.com>
2025-06-20 02:31:36 +0000

    calcutator.py

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -1,5 +1,5 @@
 num1 = int(input("Give me the first number: "))
-num2 = int(input("Give me the second number:"))
+num2 = int(input("Give me the second number: "))
 print('Thank You')
 print(f"{num1} + {num2} = {num1 + num2}")
 print(f"{num1} - {num2} = {num1 - num2}")
