2e690deafbcb2e7e859c934e1728d196848c5a8e
psaosoon <phom976@gmail.com>
2025-06-20 04:12:27 +0000

    mult.py

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,12 +1,9 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-
-num1 = int(input("Enter the frist number:"))
-num2 = int(input("Enter the second number:"))
-print(f"{num1} x {num2} = {num1 * num2}")
-if num1 * num2 > 0:
-    print("The result is positive.")
-elif num1 * num2 <0:
-    print("The result is negative.")
+num1 = int(input())
+num2 = int(input())
+result = num1 * num2
+if result > 0:
+    print("The result is positive")
+elif result < 0:
+    print("The result is negative")
 else:
-    print ("The result is positive and negative")
+    print("The result is zero")
