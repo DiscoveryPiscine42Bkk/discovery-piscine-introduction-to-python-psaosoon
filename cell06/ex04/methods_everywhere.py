4e322c7dac6daa5d82cfe74f9b7e408155445a15
psaosoon <phom976@gmail.com>
2025-06-20 14:17:15 +0000

    methods_everywhere.py

--- cell06/ex04/methods_everywhere.py
+++ cell06/ex04/methods_everywhere.py
@@ -1,16 +1,23 @@
-import sysAdd commentMore actions
-def shrink(text):
-    print(text[:8])
-def enlarge(text):
-    print(text + 'Z' * (8 - len(text)))
-args = sys.argv[1:]
-if len(args) < 1:
-    print("none")
-else:
-    for arg in args:
+import sys
+
+def shrink(s):
+    print(s[:8])
+
+def enlarge(s):
+    print(s + 'Z' * (8 - len(s)))
+
+def main():
+    if len(sys.argv) < 2:
+        print('none')
+        return
+    
+    for arg in sys.argv[1:]:
         if len(arg) > 8:
             shrink(arg)
         elif len(arg) < 8:
             enlarge(arg)
         else:
             print(arg)
+
+if __name__ == "__main__":
+    main()
