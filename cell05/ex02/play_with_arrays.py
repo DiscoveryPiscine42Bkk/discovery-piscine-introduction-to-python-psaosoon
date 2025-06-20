61b08939162048cf3c8de4dfa7c1980b5ef2016f
psaosoon <phom976@gmail.com>
2025-06-20 04:22:18 +0000

    play_with_arrays.py

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,11 +1,6 @@
 original_array = [2, 8, 9, 48, 8, 22, -12, 2]
-#filter values greater than 5
-filtered = [num for num in original_array if num > 5]
-#Remove duplicates using a set while preserving order
-unique_filtered = []
-seen = set()
-for num in filtered:
-    if num not in seen:
-        unique_filtered.append(num)
-        seen.add(num)
-print(unique_filtered)
+new_array = [x + 2 for x in original_array]
+print("original array:",original_array)
+print("new array:", new_array)
+filtered_array = [x for x in new_array if x > 5]
+print("Filtered array (>5):", filtered_array)
