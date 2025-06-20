2b55bb735d65e890e5b0dc7b7590d4f4abb6b89a
psaosoon <phom976@gmail.com>
2025-06-20 04:37:53 +0000

    cpont_it.py

--- cell05/ex11/count_it.py
+++ cell05/ex11/count_it.py
@@ -1,15 +1,8 @@
 import sys
-def main():
-
-    parameters = sys.argv[1:]
-    num_parameters = len(parameters)
-
-    if num_parameters == 0:
-         print("none")
-    else:
-         print(f"parameters: {num_parameters}")
-         for param in parameters:
-              print(f"{param}: {len(param)}")
-
-if__name__=="__main__":
-main
+if len(sys.argv) == 1:
+    print("none")
+else:
+    param_count = len(sys.argv) - 1
+    print(f"parameters: {param_count}")
+    for param in sys.argv[1:]:
+        print(f"{param} {len(param)}")
